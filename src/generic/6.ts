add0b15997bb1a78319a46d996433770edc9a749
TanyaMosk <tanya856227@gmail.com>
2023-10-05 20:20:26 +0200

    generic 6-7

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -14,8 +14,15 @@ type User = {
   password: string;
 }
 
-function createOrUpdateUser(initialValues: User) {
-  // Оновлення користувача
+function createOrUpdateUser(initialValues: Partial<User>) {
+// Оновлення користувача
+  const defaultUser: User = {
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+  };
+  return { ...defaultUser, ...initialValues };
 }
 
 createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
