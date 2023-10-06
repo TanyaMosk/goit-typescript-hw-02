dbfc11198d90d22ebcf9cfea1fc918e7114de264
TanyaMosk <tanya856227@gmail.com>
2023-10-06 14:42:37 +0200

    generic 5,8

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,14 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+  key: TKey;
+  value: TValue;
 }
 
+const one: KeyValuePair<string, number> = { key: 'one', value: 10 };
+const two: KeyValuePair<string, string> = { key: 'two', value: '15' };
+const three: KeyValuePair<string, boolean> = { key: 'three', value: true };
+
 export {};
+
